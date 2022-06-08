87c08e3a5b75da78f2da2b6effd011f7f80fd6d5
shlok1999 <09shlok1999ae1@gmail.com>
2022-06-08 15:54:49 +0530

    registration logic implemented

--- client/src/components/Registration.js
+++ client/src/components/Registration.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react'
 
 function Registration() {
     const [firstName, setFirstName] = useState("")
@@ -7,23 +7,44 @@ function Registration() {
     const [password, setPassword] = useState("")
     const [confirmPass, setConfirmPass] = useState("")
 
-    
+    const registerUser = async (e) => {
+        e.preventDefault();
+        const response = await fetch('http://localhost:2020/api/register', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                firstName, lastName, email, password, confirmPass
+            })
+        })
 
-  return (
-    <>
-    <div><h1>Registration</h1></div>
-    <form>
-        <input value={firstName} onChange={(e)=>setFirstName(e.target.value)} type="text" name="firstName" id="firstName" required /> <br />
-        <input value={lastName} onChange={(e)=>setLastName(e.target.value)} type="text" name="lastName" id="lastName" required /><br />
-        <input value={email} onChange={(e)=>setEmail(e.target.value)} type="email" name="email" id="email" required /><br />
-        <input value={password} onChange={(e)=>setPassword(e.target.value)} type="password" name="password" id="password" required /><br />
-        <input value={confirmPass} onChange={(e)=>setConfirmPass(e.target.value)} type="password" name="confirmPass" id="confirmPass" required /> <br />
-        <input type="submit" value="Register" />
-    </form>
+        const data = await response.json();
+
+        setConfirmPass("")
+        setFirstName("")
+        setLastName("")
+        setEmail("")
+        setPassword("")
+    }
 
 
-    </>
-  )
+
+    return (
+        <>
+            <div><h1>Registration</h1></div>
+            <form onSubmit={registerUser}>
+                <input value={firstName} onChange={(e) => setFirstName(e.target.value)} type="text" name="firstName" id="firstName" required /> <br />
+                <input value={lastName} onChange={(e) => setLastName(e.target.value)} type="text" name="lastName" id="lastName" required /><br />
+                <input value={email} onChange={(e) => setEmail(e.target.value)} type="email" name="email" id="email" required /><br />
+                <input value={password} onChange={(e) => setPassword(e.target.value)} type="password" name="password" id="password" required /><br />
+                <input value={confirmPass} onChange={(e) => setConfirmPass(e.target.value)} type="password" name="confirmPass" id="confirmPass" required /> <br />
+                <input type="submit" value="Register" />
+            </form>
+
+
+        </>
+    )
 }
 
 export default Registration
--- server/index.js
+++ server/index.js
@@ -1,10 +1,51 @@
 const express = require('express')
 const app = express()
+const User = require('./models/user.model')
+const cors = require('cors')
+app.use(cors())
+app.use(express.json())
 
-app.get('/', (req, res)=>{
-    res.send("Hello")
+const mongoose = require('mongoose')
+
+mongoose.connect('mongodb://localhost:27017/AvenueUser', () => {
+    console.log("Connected")
 })
 
-app.listen(2020, ()=>{
+app.get('/', (req, res) => {
+    res.send("Hello")
+})
+app.post('/api/register', async (req, res) => {
+    // console.log(req.body)
+    try {
+        const user = await User.create({
+            firstName: req.body.firstName,
+            lastName: req.body.lastName,
+            email: req.body.email,
+            password: req.body.password,
+            confirmPass: req.body.confirmPass
+        })
+        res.json({ status: 'ok' })
+    } catch(err) {
+        res.json({ status: 'error', error: 'Duplicate Email' })
+    }
+})
+
+app.post('/api/login', async (req, res) => {
+    const user = await User.findOne({
+        email: req.body.email,
+        password: req.body.password
+    })
+
+    if(user){
+        return res.json({status: 'ok', user: true})
+    }else{
+        return res.json({status: 'error', user: false})
+
+    }
+})
+
+
+
+app.listen(2020, () => {
     console.log(`http://localhost:2020`)
 })
