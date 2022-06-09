cc5f7eea02e1a80dc72045c13122dfb1f17d9649
shlok1999 <09shlok1999ae1@gmail.com>
2022-06-09 07:44:01 +0530

    designed and authenticated pages

--- server/index.js
+++ server/index.js
@@ -5,6 +5,8 @@ const User = require('./models/user.model')
 const cors = require('cors')
 app.use(cors())
 app.use(express.json())
+const bcrypt = require('bcryptjs')
+
 
 const mongoose = require('mongoose')
 
@@ -12,24 +14,36 @@ mongoose.connect('mongodb://localhost:27017/AvenueUser', () => {
     console.log("Connected")
 })
 
+
 app.get('/', (req, res) => {
     res.send("Hello")
 })
+
+// REGISTER PAGE
 app.post('/api/register', async (req, res) => {
     // console.log(req.body)
     try {
-        const user = await User.create({
-            firstName: req.body.firstName,
-            lastName: req.body.lastName,
-            email: req.body.email,
-            password: req.body.password,
-            confirmPass: req.body.confirmPass
-        })
-        res.json({ status: 'ok' })
+        
+        const password = req.body.password;
+        const confirmPass = req.body.confirmPass;
+        let hash = crypto.createHash('md5').update(password).digest('hex');
+        if(password === confirmPass){
+            // Create User
+            const user = await User.create({
+                firstName: req.body.firstName,
+                lastName: req.body.lastName,
+                email: req.body.email,
+                password: req.body.password,
+                confirmPass: req.body.confirmPass
+            })
+        }
+        
     } catch(err) {
         res.json({ status: 'error', error: 'Duplicate Email' })
     }
 })
+
+// LOGIN PAGE
 
 app.post('/api/login', async (req, res) => {
     const user = await User.findOne({
@@ -44,7 +58,7 @@ app.post('/api/login', async (req, res) => {
             email: user.email,
             
         }, 'secret')
-        // console.log(token)
+        console.log(token)
         return res.json({status: 'ok', user: token})
     }else{
         return res.json({status: 'error', user: false})
@@ -52,6 +66,13 @@ app.post('/api/login', async (req, res) => {
     }
 })
 
+
+// const securePassword =async (password)=>{
+//     const hashed = await bcrypt.hash(password, 10)
+//     console.log(hashed)
+// }
+
+// securePassword('shlok@123')
 
 
 app.listen(2020, () => {
--- server/models/user.model.js
+++ server/models/user.model.js
@@ -1,13 +1,24 @@
 const mongoose = require('mongoose')
-
+const bcrypt = require('bcryptjs')
 const User = mongoose.Schema({
     firstName: {type: String, required: true},
     lastName: {type: String, required: true},
     email: {type: String, required: true, unique: true},
-    password: {type: String, required: true},
+    password: {type: String, required: true, minlength: 3},
     confirmPass: {type: String, required: true}
 },{
     collection: 'user-data'
+})
+
+User.pre("create", async function (next){
+    if(this.isModified("password")){
+        const passHash = await bcrypt.hash(password, 10);
+        this.password = await bcrypt.hash(this.password, 10)
+        console.log(this.password)
+    }
+    next();
+
+    
 })
 
 const model =  mongoose.model('UserData', User)
